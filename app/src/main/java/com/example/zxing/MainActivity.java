f7eb38e11c95e7781d06b0c4d2bc7e6f88419ec4
linhaitao <1127141134@qq.com>
2020-07-24 16:54:44 +0800

    添加相机申请权限

--- app/src/main/java/com/example/zxing/MainActivity.java
+++ app/src/main/java/com/example/zxing/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.zxing;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.zxing.client.android.CaptureActivity;
 
@@ -24,9 +29,20 @@ public class MainActivity extends AppCompatActivity {
         bt_scan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                //去寻找是否已经有了相机的权限
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+                    //Toast.makeText(MainActivity.this,"您申请了动态权限",Toast.LENGTH_SHORT).show();
+                    //如果有了相机的权限有调用相机
+                    Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
 //                intent.setAction(Intents.Scan.ACTION);
-                startActivityForResult(intent,1);
+                    startActivityForResult(intent, 1);
+
+                } else {
+                    //否则去请求相机权限
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 100);
+
+                }
             }
         });
 
@@ -47,15 +63,52 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        }
 
-        Log.e("Main","1");
-        if (requestCode == 1){
-            Log.e("Main","2");
+        Log.e("Main", "1");
+        if (requestCode == 1) {
+            Log.e("Main", "2");
             if (data != null) {
-                Log.e("Main","3");
+                Log.e("Main", "3");
                 String string = data.getStringExtra("displayContents");
                 Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
             }
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            //如果requestCode为100 就走这里
+            case 100:
+
+                //permissions[0].equals(Manifest.permission.CAMERA)
+                //grantResults[0] == PackageManager.PERMISSION_GRANTED
+                //上面的俩个判断可有可无
+                if (permissions[0].equals(Manifest.permission.CAMERA)) {
+
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        //如果用户同意了再去打开相机
+                        Toast.makeText(MainActivity.this, "允许申请权限", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+//                intent.setAction(Intents.Scan.ACTION);
+                        startActivityForResult(intent, 1);
+
+                    } else {
+                        //因为第一次的对话框是系统提供的 从这以后系统不会自动弹出对话框 我们需要自己弹出一个对话框
+                        //进行询问的工作
+//                        startAlertDiaLog();
+                        Toast.makeText(this, "拒绝申请权限", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+
+                break;
+
+        }
+
+    }
+
 
 }
